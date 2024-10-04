4fc3351040a9dbbcc644b12c3ce72b6a067dcd41
公司电脑1 <1@qq.com>
2024-10-04 13:08:10 +0800

    已实现多页面模板

--- app/src/main/java/com/example/ec200a_um982_app/MainActivity.java
+++ app/src/main/java/com/example/ec200a_um982_app/MainActivity.java
@@ -1,24 +1,107 @@
 package com.example.ec200a_um982_app;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+import com.example.ec200a_um982_app.main_fragment.BluetoothFragment;
+import com.example.ec200a_um982_app.main_fragment.NtripFragment;
+import com.example.ec200a_um982_app.main_fragment.Um982Fragment;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
 
+    private BottomNavigationView mNavigationView;
+    private FragmentManager mFragmentManager;
+    private Fragment[] fragments;
+    private int lastFragment;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        setupNavigationView();
+        // 延迟加载其他Fragment以避免崩溃
+        new Handler().postDelayed(this::loadOtherFragments, 500);
+    }
+
+    private void setupNavigationView() {
+        mNavigationView = findViewById(R.id.main_navigation_bar);
+        initFragment();
+        initListener();
+    }
+    private void loadOtherFragments() {
+        FragmentTransaction transaction = mFragmentManager.beginTransaction();
+        for (int i = 1; i < fragments.length; i++) {
+            if (!fragments[i].isAdded()) {
+                transaction.add(R.id.main_page_controller, fragments[i]);
+                transaction.hide(fragments[i]);
+            }
+        }
+        transaction.commitAllowingStateLoss();
+    }
+    private void initFragment() {
+        BluetoothFragment mBluetoothFragment = new BluetoothFragment();
+        NtripFragment mNtripFragment = new NtripFragment();
+        Um982Fragment mWebFragment = new Um982Fragment();
+        fragments = new Fragment[]{mBluetoothFragment, mNtripFragment, mWebFragment};
+        mFragmentManager = getSupportFragmentManager();
+        lastFragment = 0;
+        mFragmentManager.beginTransaction()
+                .replace(R.id.main_page_controller, mBluetoothFragment)
+                .show(mBluetoothFragment)
+                .commit();
+    }
+
+    private void initListener() {
+        mNavigationView.setOnNavigationItemSelectedListener(item -> {
+            int i = item.getItemId();
+            if (i == R.id.Bluetooth && lastFragment != 0) {
+                switchFragment(lastFragment, 0);
+                lastFragment = 0;
+                return true;
+            } else if (i == R.id.Ntrip && lastFragment != 1) {
+                switchFragment(lastFragment, 1);
+                lastFragment = 1;
+                return true;
+            } else if (i == R.id.Um982 && lastFragment != 2) {
+                switchFragment(lastFragment, 2);
+                lastFragment = 2;
+                return true;
+            }
+            return false;
         });
+    }
+    private void switchFragment(int lastFragment, int index) {
+        FragmentTransaction transaction = mFragmentManager.beginTransaction();
+        transaction.hide(fragments[lastFragment]);
+        if (!fragments[index].isAdded()) {
+            transaction.add(R.id.main_page_controller, fragments[index]);
+        }
+        transaction.show(fragments[index]).commitAllowingStateLoss();
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
     }
 }

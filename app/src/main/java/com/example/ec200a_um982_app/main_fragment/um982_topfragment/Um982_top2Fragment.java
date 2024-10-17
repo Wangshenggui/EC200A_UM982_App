5ae18b1568c4b2518418c53caa1284067cc6da95
公司电脑1 <1@qq.com>
2024-10-17 13:04:19 +0800

    在网页端正常显示

--- app/src/main/java/com/example/ec200a_um982_app/main_fragment/um982_topfragment/Um982_top2Fragment.java
+++ app/src/main/java/com/example/ec200a_um982_app/main_fragment/um982_topfragment/Um982_top2Fragment.java
@@ -2,11 +2,9 @@ package com.example.ec200a_um982_app.main_fragment.um982_topfragment;
 
 import android.annotation.SuppressLint;
 import android.os.Bundle;
-
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
-
 import android.os.Handler;
 import android.os.Looper;
 import android.util.Log;
@@ -16,8 +14,7 @@ import android.view.ViewGroup;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
-import android.widget.TextView;
-
+import android.widget.Toast;
 import com.example.ec200a_um982_app.MainActivity;
 import com.example.ec200a_um982_app.R;
 import com.example.ec200a_um982_app.SharedViewModel;
@@ -25,71 +22,18 @@ import com.example.ec200a_um982_app.SocketService;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Objects;
 
-import android.view.View;
-import android.view.ViewGroup;
-import android.content.Context;
-import android.webkit.JavascriptInterface;
-import android.widget.Toast;
-
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link Um982_top2Fragment#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class Um982_top2Fragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
 
-
     private SharedViewModel viewModel1;
-
-    TextView DatumCodeText;
-    TextView LatOffsetText;
-    TextView LonOffsetText;
-    TextView AltOffsetText;
-    TextView RfDatumCodeText;
-    TextView UTCText;
-    TextView LatExpText;
-    TextView LonExpText;
-    TextView AltExpText;
-    TextView LatText;
-    TextView LatDirText;
-    TextView LonText;
-    TextView LonDirText;
-    TextView RtkModeText;
-    TextView SatsText;
-    TextView hdopText;
-    TextView AltText;
-    TextView a_unitsText;
-    TextView DiffDataAgeText;
-    TextView stnIDText;
-    TextView statusText;
-    TextView TrackTrueText;
-    TextView speedText;
-    TextView dateText;
-    TextView magVarText;
-    TextView varDirText;
-    TextView rateText;
-    TextView HeadingText;
-
-    String DatumCode="";
-
-
     private WebView webView;
-
-    // Timer variables
     private Handler handler;
     private Runnable timerRunnable;
-    private static final int TIMER_INTERVAL = 1000; // 1 seconds
+    private static final int TIMER_INTERVAL = 1000; // 1 second
 
-
-
-    // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
 
@@ -97,15 +41,6 @@ public class Um982_top2Fragment extends Fragment {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment Um982_top2Fragment.
-     */
-    // TODO: Rename and change types and number of parameters
     public static Um982_top2Fragment newInstance(String param1, String param2) {
         Um982_top2Fragment fragment = new Um982_top2Fragment();
         Bundle args = new Bundle();
@@ -125,74 +60,33 @@ public class Um982_top2Fragment extends Fragment {
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_um982_top2, container, false);
-
-        DatumCodeText = view.findViewById(R.id.DatumCodeText);
-        LatOffsetText = view.findViewById(R.id.LatOffsetText);
-        LonOffsetText = view.findViewById(R.id.LonOffsetText);
-        AltOffsetText = view.findViewById(R.id.AltOffsetText);
-        RfDatumCodeText = view.findViewById(R.id.RfDatumCodeText);
-        UTCText = view.findViewById(R.id.UTCText);
-        LatExpText = view.findViewById(R.id.LatExpText);
-        LonExpText = view.findViewById(R.id.LonExpText);
-        AltExpText = view.findViewById(R.id.AltExpText);
-        LatText = view.findViewById(R.id.LatText);
-        LatDirText = view.findViewById(R.id.LatDirText);
-        LonText = view.findViewById(R.id.LonText);
-        LonDirText = view.findViewById(R.id.LonDirText);
-        RtkModeText = view.findViewById(R.id.RtkModeText);
-        SatsText = view.findViewById(R.id.SatsText);
-        hdopText = view.findViewById(R.id.hdopText);
-        AltText = view.findViewById(R.id.AltText);
-        a_unitsText = view.findViewById(R.id.a_unitsText);
-        DiffDataAgeText = view.findViewById(R.id.DiffDataAgeText);
-        stnIDText = view.findViewById(R.id.stnIDText);
-        statusText = view.findViewById(R.id.statusText);
-        TrackTrueText = view.findViewById(R.id.TrackTrueText);
-        speedText = view.findViewById(R.id.speedText);
-        dateText = view.findViewById(R.id.dateText);
-        magVarText = view.findViewById(R.id.magVarText);
-        varDirText = view.findViewById(R.id.varDirText);
-        rateText = view.findViewById(R.id.rateText);
-        HeadingText = view.findViewById(R.id.HeadingText);
-
-
         webView = view.findViewById(R.id.webView);
 
         WebSettings webSettings = webView.getSettings();
         webSettings.setJavaScriptEnabled(true);  // Enable JavaScript
-
-        // Add JavaScript interface to communicate with the WebView
         webView.addJavascriptInterface(new WebAppInterface(), "Android");
-
-        // Ensure WebView stays inside the app instead of launching a browser
         webView.setWebViewClient(new WebViewClient());
-
-        // Load the local HTML file
         webView.loadUrl("file:///android_asset/Um982_top2Fragment.html");
 
-        // Initialize Handler
         handler = new Handler(Looper.getMainLooper());
-
-        // Define the task to be run periodically
         timerRunnable = new Runnable() {
-            @SuppressLint("SetTextI18n")
             @Override
             public void run() {
 
+//                MainActivity.showToast(getActivity(),"12");
+//                sendDataToWebView("$GNGGA,020523.000,2623.013809,N,10636.512344,E,5,33,1.26,1199.9,M,-26.1,M,1.0,0451*4A");
 
                 // Repeat the task every TIMER_INTERVAL milliseconds
                 handler.postDelayed(this, TIMER_INTERVAL);
             }
         };
+        handler.post(timerRunnable); // 启动更新
 
         return view;
     }
 
-    // JavaScript Interface class to allow communication from the WebView to Android
     public class WebAppInterface {
         @android.webkit.JavascriptInterface
         public void showToast(String toast) {
@@ -201,12 +95,76 @@ public class Um982_top2Fragment extends Fragment {
 
         @android.webkit.JavascriptInterface
         public void sendDataToAndroid(String data) {
-            // Log the data received from WebView
             Log.d("WebAppInterface", "Data received from WebView: " + data);
-            // Display a toast message with the received data
-            MainActivity.showToast(getActivity(),data);
+            MainActivity.showToast(getActivity(), "你干嘛" + data);
         }
     }
+
+    public void sendDataToWebView(String data) {
+        webView.evaluateJavascript("javascript:receiveDataFromAndroid('" + data + "')", null);
+    }
+
+    private StringBuilder dataBuffer = new StringBuilder();
+    @Override
+    public void onViewCreated(View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        viewModel1 = new ViewModelProvider(requireActivity()).get(SharedViewModel.class);
+        viewModel1.getDataGroup1().observe(getViewLifecycleOwner(), new Observer<String>() {
+            @Override
+            public void onChanged(String data) {
+                // 将接收到的数据添加到缓冲区
+                dataBuffer.append(data);
+
+                // 检查是否包含 \r\n
+                while (dataBuffer.indexOf("\r\n") != -1) {
+                    String frames = dataBuffer.toString();
+                    String[] messageArray = frames.split("::");
+                    // 遍历输出拆分后的消息
+                    for (String message : messageArray) {
+                        // 处理接收到的数据
+                        if (message != null && message.startsWith("$GNDTM,")) {
+                            sendDataToWebView(message);
+                        } else if (message != null && message.startsWith("$GNGBS,")) {
+                            sendDataToWebView(message);
+                        } else if (message != null && message.startsWith("$GNGGA,")) {
+                            sendDataToWebView(message);
+                            SocketService.CORSSSGString = message;
+                        } else if (message != null && message.startsWith("$GNGGAH,")) {
+                            sendDataToWebView(message);
+                        } else if (message != null && message.startsWith("$GNGLL,")) {
+                            sendDataToWebView(message);
+                        } else if (message != null && message.startsWith("$GNGLLH,")) {
+                            sendDataToWebView(message);
+                        } else if (message != null && message.startsWith("$GNGNS,")) {
+                            sendDataToWebView(message);
+                        } else if (message != null && message.startsWith("$GNGNSH,")) {
+                            sendDataToWebView(message);
+                        } else if (message != null && message.startsWith("$GNGST,")) {
+                            sendDataToWebView(message);
+                        } else if (message != null && message.startsWith("$GNGSTH,")) {
+                            sendDataToWebView(message);
+                        } else if (message != null && message.startsWith("$GNTHS,")) {
+                            sendDataToWebView(message);
+                        } else if (message != null && message.startsWith("$GNRMC,")) {
+                            sendDataToWebView(message);
+                        } else if (message != null && message.startsWith("$GNRMCH,")) {
+                            sendDataToWebView(message);
+                        } else if (message != null && message.startsWith("$GNROT,")) {
+                            sendDataToWebView(message);
+                        } else if (message != null && message.startsWith("$GNVTG,")) {
+                            sendDataToWebView(message);
+                        } else if (message != null && message.startsWith("$GNVTGH,")) {
+                            sendDataToWebView(message);
+                        } else if (message != null && message.startsWith("$GNZDA,")) {
+                            sendDataToWebView(message);
+                        }
+                    }
+                    dataBuffer.setLength(0);
+                }
+            }
+        });
+    }
+
 
     public static List<String> getSpecificSubstrings(String input, int[] indices) {
         List<String> result = new ArrayList<>();
@@ -224,139 +182,5 @@ public class Um982_top2Fragment extends Fragment {
         }
 
         return result; // 返回提取的部分
-    }
-
-    //    private StringBuilder dataBuffer = new StringBuilder();
-    @Override
-    public void onViewCreated(View view, Bundle savedInstanceState) {
-        super.onViewCreated(view, savedInstanceState);
-        viewModel1 = new ViewModelProvider(requireActivity()).get(SharedViewModel.class);
-        viewModel1.getDataGroup1().observe(getViewLifecycleOwner(), new Observer<String>() {
-            @Override
-            public void onChanged(String data) {
-                // 将接收到的数据添加到缓冲区
-//                dataBuffer.append(data);
-
-//                // 检查是否包含 \r\n
-//                while (dataBuffer.indexOf("\r\n") != -1) {
-//                    String frames = dataBuffer.toString();
-//                    String[] messageArray = frames.split("::");
-//                    // 遍历输出拆分后的消息
-//                    for (String message : messageArray) {
-//                        // 处理接收到的数据
-//                        if (message != null && message.startsWith("$GNDTM,")) {
-//                            int[] indices = {2, 4, 6,8,9};
-//                            List<String> results = getSpecificSubstrings(message, indices);
-//
-//                            DatumCodeText.setText("本地坐标系代码: " + results.get(0));
-//                            LatOffsetText.setText("纬度偏移量: " + results.get(1) + "′");
-//                            LonOffsetText.setText("经度偏移量: " + results.get(2) + "′");
-//                            AltOffsetText.setText("海拔偏移量: " + results.get(3) + " m");
-//                            RfDatumCodeText.setText("参考坐标系代码: " + results.get(4).substring(0,3));
-//                        } else if (message != null && message.startsWith("$GNGBS,")) {
-//                            int[] indices = {2,3,4,5};
-//                            List<String> results = getSpecificSubstrings(message, indices);
-//
-//                            UTCText.setText("UTC时间: " + results.get(0));
-//                            LatExpText.setText("纬度预期误差: " + results.get(1) + " m");
-//                            LonExpText.setText("经度预期误差: " + results.get(2) + " m");
-//                            AltExpText.setText("海拔预期误差: " + results.get(3) + " m");
-//                        } else if (message != null && message.startsWith("$GNGGA,")) {
-//                            int[] indices = {3,4,5,6,7,8,9,10,11,14,15};
-//                            List<String> results = getSpecificSubstrings(message, indices);
-//
-//                            LatText.setText("纬度: " + results.get(0));
-//                            LatDirText.setText("纬度方向: " + results.get(1));
-//                            LonText.setText("经度: " + results.get(2));
-//                            LonDirText.setText("经度方向: " + results.get(3));
-//                            if(Objects.equals(results.get(4), "0"))
-//                                RtkModeText.setText("定位模式: " + "定位无效");
-//                            else if(Objects.equals(results.get(4), "1"))
-//                                RtkModeText.setText("定位模式: " + "单点定位");
-//                            else if(Objects.equals(results.get(4), "2"))
-//                                RtkModeText.setText("定位模式: " + "差分定位");
-//                            else if(Objects.equals(results.get(4), "3"))
-//                                RtkModeText.setText("定位模式: " + "GPS PPS 模式");
-//                            else if(Objects.equals(results.get(4), "4"))
-//                                RtkModeText.setText("定位模式: " + "固定RTK模式");
-//                            else if(Objects.equals(results.get(4), "5"))
-//                                RtkModeText.setText("定位模式: " + "浮动RTK模式");
-//                            else if(Objects.equals(results.get(4), "6"))
-//                                RtkModeText.setText("定位模式: " + "惯导模式");
-//                            else if(Objects.equals(results.get(4), "7"))
-//                                RtkModeText.setText("定位模式: " + "手动输入模式");
-//                            else if(Objects.equals(results.get(4), "8"))
-//                                RtkModeText.setText("定位模式: " + "模拟器模式");
-//
-//                            SatsText.setText("使用中的卫星数: " + results.get(5));
-//                            hdopText.setText("水平精度因子: " + results.get(6));
-//                            AltText.setText("海拔高度: " + results.get(7));
-//                            a_unitsText.setText("海拔高度单位: " + results.get(8));
-//                            DiffDataAgeText.setText("差分数据龄期: " + results.get(9));
-//                            if(results.get(10).length()>3) {
-//                                stnIDText.setText("差分基站ID: " + results.get(10).substring(0,3));
-//                            }
-//
-//                        } else if (message != null && message.startsWith("$GNGGAH,")) {
-//
-//                        } else if (message != null && message.startsWith("$GNGLL,")) {
-//
-//                        } else if (message != null && message.startsWith("$GNGLLH,")) {
-//
-//                        } else if (message != null && message.startsWith("$GNGNS,")) {
-//
-//                        } else if (message != null && message.startsWith("$GNGNSH,")) {
-//
-//                        } else if (message != null && message.startsWith("$GNGRS,")) {
-//
-//                        } else if (message != null && message.startsWith("$GNGRSH,")) {
-//
-//                        } else if (message != null && message.startsWith("$GNGSA,")) {
-//
-//                        } else if (message != null && message.startsWith("$GNGSAH,")) {
-//
-//                        } else if (message != null && message.startsWith("$GNGST,")) {
-//
-//                        } else if (message != null && message.startsWith("$GNGSTH,")) {
-//
-//                        } else if (message != null && message.startsWith("$GNGSV,")) {
-//
-//                        } else if (message != null && message.startsWith("$GNGSVH,")) {
-//
-//                        } else if (message != null && message.startsWith("$GNTHS,")) {
-//                            int[] indices = {2};
-//                            List<String> results = getSpecificSubstrings(message, indices);
-//
-//                            HeadingText.setText("天线方向: " + results.get(0));
-//                        } else if (message != null && message.startsWith("$GNRMC,")) {
-//                            int[] indices = {14,8,9,10,11,12};
-//                            List<String> results = getSpecificSubstrings(message, indices);
-//
-//                            statusText.setText("导航状态指示: " + results.get(0).substring(0,1));
-//                            float speed = Float.parseFloat(results.get(1));
-//                            speedText.setText("地面速率: " + speed*1.852 + "km/h\r\n\t" + speed*0.5144 + "m/s");
-//                            TrackTrueText.setText("地面航向: " + results.get(2));
-//                            dateText.setText("日期: " + results.get(3));
-//                            magVarText.setText("磁偏角: " + results.get(4));
-//                            varDirText.setText("磁偏角方向: " + results.get(5));
-//                        } else if (message != null && message.startsWith("$GNRMCH,")) {
-//
-//                        } else if (message != null && message.startsWith("$GNROT,")) {
-//                            int[] indices = {2};
-//                            List<String> results = getSpecificSubstrings(message, indices);
-//
-//                            rateText.setText("旋转速率: " + results.get(0) + "度/分");
-//                        } else if (message != null && message.startsWith("$GNVTG,")) {
-//
-//                        } else if (message != null && message.startsWith("$GNVTGH,")) {
-//
-//                        } else if (message != null && message.startsWith("$GNZDA,")) {
-//
-//                        }
-//                    }
-//                    dataBuffer.setLength(0);
-//                }
-            }
-        });
     }
 }
